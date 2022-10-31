3e5bfefafe9fb39f69b386fb72899120b542814e
thorn1113 <1902492516@qq.com.cn>
2022-10-31 09:34:26 +0800

    gateway限流

---
+++ xq_pug_gateway/src/main/java/com/pug/gateway/limit/Raonfiguration.java
@@ -0,0 +1,36 @@
+package com.pug.gateway.limit;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import reactor.core.publisher.Mono;
+
+public class Raonfiguration {
+
+    @Bean
+    public KeyResolver pathKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest()
+                        .getPath()
+                        .toString()
+        );
+    }
+
+    @Bean
+    @Primary
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostString());
+    }
+
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("userId"));
+    }
+
+
+    @Bean
+    public KeyResolver apiKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getPath().value());
+    }
+}
